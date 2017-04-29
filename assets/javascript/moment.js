4b53000b45991835cbccd79a4491989c02047972
Carlos Perez <solracias@gmail.com>
2017-04-29 09:56:23 -0700

    starter files are now set up with links and src

---
+++ assets/javascript/moment.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCdJzK9_TremO4_gA18kXt91Odalr5hSvc",
+    authDomain: "myfirstproject-b04dd.firebaseapp.com",
+    databaseURL: "https://myfirstproject-b04dd.firebaseio.com",
+    projectId: "myfirstproject-b04dd",
+    storageBucket: "myfirstproject-b04dd.appspot.com",
+    messagingSenderId: "94314040395"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
